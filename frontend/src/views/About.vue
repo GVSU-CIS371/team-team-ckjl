13921262a9e2806a93961eda866361d3f9bcc0c1
Kyle-Gordon <gordon.kyle1@student.oaklandcc.edu>
2026-04-09 15:36:57 -0400

    About.vue + CSS Updates
    I added all the content to About.Vue, along with new CSS rules to make it look good. I'd love a second opinion on it, if anyone has the time

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -49,9 +49,58 @@ const { user } = useAuth()
 			</nav>
 		</header>
 
-		<main>
-			<h3>About Us</h3>
-		</main>
+	<main class="oswald-regular">
+		<h2>About Occurency</h2>
+		
+		<br>
+		
+		<!-- Our Story -->
+		<h2>Our Story</h2>
+		<div>
+			
+				<p>
+				We started as a team of 4 friends who wanted to bring our events to a wider audience, which then evolved to a little side project called "Occurency". 
+				We wanted to create a platform that would make it easy for people to discover and attend events in their local area, and to connect with others who share their interests. 
+				</p>
+			
+				<br>
+				
+				<p>And in the end, we succeeded.</p>
+				
+				<br>
+				
+				<p>
+				Occurency is a software service designed to bring people together for unforgettable experiences. 
+				Using our platform, users can create and find events that match their interests, whether it's a local concert, a block party, or soemthing entirely new. 
+				</p>
+				<img class="tablet-desktop abt-img" src="images/occurency_abt_OS_SI2.jpg" alt="Occurency developers sitting around a laptop">
+		</div>
+		
+		<!-- Our Mission -->
+		<h2>Our Mission</h2>
+		<div>
+				<p>
+				Our mission is to help people create lasting memories by connecting them with events that inspire and excite them. 
+				At Occurency, our belief is that life is about the experiences and memories we share with others, and we're here to make those memories happen.
+				</p>
+				
+				<p>
+				We strive to build a welcoming and inclusive platform where anyone can discover something new, meet like-minded individuals, and turn everyday moments into meaningful experiences.
+				</p>
+				<img class="tablet-desktop abt-img" src="images/occurency_abt_OM_SI1.jpg" alt="A group of people sitting happily around the dining table at an event planned with Occurency">
+		</div>
+
+		<!-- Our Team (Tablet/Desktop Only) -->
+		<div class="tablet-desktop">
+			<h2>Our Team</h2>
+				<p>
+				Our team is passionate about creating a user-friendly platform that fosters community and engagement, and we're committed to providing excellent customer service and support to our users.
+				</p>
+				<p>The team consists of 4 members:</p>
+				<p><b>Chad - Event & RSVP Coordinator</b> | <b>Kyle - Frontend Developer</b> | <b>Josh - User & Login Specialist</b> | <b>Leon - Community Builder</b></p>
+				<p>Together, we're dedicated to making Occurency the go-to platform for event discovery and creation, and we can't wait to see the amazing memories that our users will create with us.</p>
+		</div>	
+	</main>
 
 		<footer class="oswald-regular">
 
